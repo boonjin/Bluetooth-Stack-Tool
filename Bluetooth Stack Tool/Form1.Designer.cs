ac2f41422fa23584315b9a96425c357abc4bc197
Boon Jin <jingoh@gmail.com>
2015-06-11 19:50:54 +0800

    working update for inf

--- Bluetooth Stack Tool/Form1.Designer.cs
+++ Bluetooth Stack Tool/Form1.Designer.cs
@@ -28,12 +28,87 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.buttonRetrieveDevices = new System.Windows.Forms.Button();
+            this.buttonRunInstaller = new System.Windows.Forms.Button();
+            this.buttonUpdateINF = new System.Windows.Forms.Button();
+            this.textBoxLog = new System.Windows.Forms.TextBox();
+            this.buttonPatch = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // buttonRetrieveDevices
+            // 
+            this.buttonRetrieveDevices.Location = new System.Drawing.Point(12, 12);
+            this.buttonRetrieveDevices.Name = "buttonRetrieveDevices";
+            this.buttonRetrieveDevices.Size = new System.Drawing.Size(114, 34);
+            this.buttonRetrieveDevices.TabIndex = 0;
+            this.buttonRetrieveDevices.Text = "Retrieve Devices";
+            this.buttonRetrieveDevices.UseVisualStyleBackColor = true;
+            this.buttonRetrieveDevices.Click += new System.EventHandler(this.buttonRetrieveDevices_Click);
+            // 
+            // buttonRunInstaller
+            // 
+            this.buttonRunInstaller.Location = new System.Drawing.Point(12, 52);
+            this.buttonRunInstaller.Name = "buttonRunInstaller";
+            this.buttonRunInstaller.Size = new System.Drawing.Size(114, 34);
+            this.buttonRunInstaller.TabIndex = 1;
+            this.buttonRunInstaller.Text = "Run Toshiba Installer";
+            this.buttonRunInstaller.UseVisualStyleBackColor = true;
+            this.buttonRunInstaller.Click += new System.EventHandler(this.buttonRunInstaller_Click);
+            // 
+            // buttonUpdateINF
+            // 
+            this.buttonUpdateINF.Location = new System.Drawing.Point(12, 92);
+            this.buttonUpdateINF.Name = "buttonUpdateINF";
+            this.buttonUpdateINF.Size = new System.Drawing.Size(114, 34);
+            this.buttonUpdateINF.TabIndex = 2;
+            this.buttonUpdateINF.Text = "Update INF";
+            this.buttonUpdateINF.UseVisualStyleBackColor = true;
+            this.buttonUpdateINF.Click += new System.EventHandler(this.buttonUpdateINF_Click);
+            // 
+            // textBoxLog
+            // 
+            this.textBoxLog.Location = new System.Drawing.Point(132, 12);
+            this.textBoxLog.Multiline = true;
+            this.textBoxLog.Name = "textBoxLog";
+            this.textBoxLog.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBoxLog.Size = new System.Drawing.Size(304, 229);
+            this.textBoxLog.TabIndex = 3;
+            // 
+            // buttonPatch
+            // 
+            this.buttonPatch.Location = new System.Drawing.Point(12, 132);
+            this.buttonPatch.Name = "buttonPatch";
+            this.buttonPatch.Size = new System.Drawing.Size(114, 34);
+            this.buttonPatch.TabIndex = 4;
+            this.buttonPatch.Text = "Patch Registry";
+            this.buttonPatch.UseVisualStyleBackColor = true;
+            this.buttonPatch.Click += new System.EventHandler(this.buttonPatch_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(459, 261);
+            this.Controls.Add(this.buttonPatch);
+            this.Controls.Add(this.textBoxLog);
+            this.Controls.Add(this.buttonUpdateINF);
+            this.Controls.Add(this.buttonRunInstaller);
+            this.Controls.Add(this.buttonRetrieveDevices);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button buttonRetrieveDevices;
+        private System.Windows.Forms.Button buttonRunInstaller;
+        private System.Windows.Forms.Button buttonUpdateINF;
+        private System.Windows.Forms.TextBox textBoxLog;
+        private System.Windows.Forms.Button buttonPatch;
     }
 }
 
--- Bluetooth Stack Tool/Form1.cs
+++ Bluetooth Stack Tool/Form1.cs
@@ -7,44 +7,241 @@ using System.Linq;
 using System.Text;
 using System.Windows.Forms;
 using System.Management;
+using System.IO;
+using Microsoft.Win32;
 
 namespace Bluetooth_Stack_Tool
 {
     public partial class Form1 : Form
     {
+        private string INF_PATH = @"C:\Program Files (x86)\Toshiba\Bluetooth Toshiba Stack\Drivers\tosrfusb\tosrfusb.inf";
+        //private string INF_PATH = Directory.GetCurrentDirectory() + @"\tosrfusb.inf";
+
+        private List<string> deviceListID = new List<string>();
+        private List<string> deviceListName = new List<string>();
+
         public Form1()
         {
-            InitializeComponent();
+            InitializeComponent();      
+        }
 
+        private void Form1_Load(object sender, EventArgs e)
+        {
             retrieveVIDPID();
+        }
+        private void writeLog(string message)
+        {
+             BeginInvoke((MethodInvoker)delegate()
+            {
+                textBoxLog.Text += System.Environment.NewLine + message;
+                textBoxLog.SelectionStart = textBoxLog.TextLength;
+                textBoxLog.ScrollToCaret();
+            });
+        }
+
+        private void buttonRetrieveDevices_Click(object sender, EventArgs e)
+        {
+            retrieveVIDPID();
+        }
+
+        private void buttonRunInstaller_Click(object sender, EventArgs e)
+        {
+            runToshibaInstaller();
+        }
+
+        private void buttonUpdateINF_Click(object sender, EventArgs e)
+        {
+            updateINF();
+        }
+
+        private void buttonPatch_Click(object sender, EventArgs e)
+        {
+            patchRegistry();
+
         }
 
         private void retrieveVIDPID()
         {
             ManagementClass class1 = new ManagementClass("Win32_PnPEntity");
-
-
+            
             foreach (ManagementObject ob in class1.GetInstances())
             {
-                // PropertyDataCollection properties = ob.Properties;
-
                 Object obj = ob["Name"];
                 if (obj == null) continue;
 
                 string objName = obj.ToString();
-                if (objName.Contains("Bluetooth"))
+                if (objName.ToLower().Contains("bluetooth"))
                 {
                     Object objDescription = ob["DeviceID"];
 
                     if (objDescription != null && objDescription.ToString().IndexOf(@"USB\VID_") == 0)
                     {
-                        Console.WriteLine(ob["Name"]);
-                        Console.WriteLine(ob["DeviceID"]);
-
-                        MessageBox.Show(objName + "  " + objDescription);
+                        string objDeviceID = objDescription.ToString().Substring(0, 21);
+                        deviceListID.Add(objDeviceID);
+                        deviceListName.Add(objName);
+                        writeLog("Device found: ");
+                        writeLog(objName);
+                        writeLog(objDeviceID);
+                        writeLog("");
+                       // MessageBox.Show(objName + "  " + objDescription);
                     }
                 }
             }
+
+            if (deviceListID.Count == 0)
+            {
+                writeLog("No Bluetooth device found");
+            }
         }
+
+        private void runToshibaInstaller()
+        {
+            writeLog("Running Toshiba Bluetooth Stack Installer");
+
+            try
+            {
+
+                writeLog("Run - Complete");
+            }
+            catch (Exception ex)
+            {
+                writeLog("Run - Failed : " + ex.ToString());
+            }
+        }
+
+
+        private void updateINF()
+        {
+            writeLog("Updating INF");
+            try
+            {
+                //back up tosrfusb.inf before proceeding
+                writeLog("Back up INF");
+                if (!File.Exists(INF_PATH))
+                {
+                    writeLog("Unable to find tosrfusb.inf - Waiting for file to be created");
+
+                    FileSystemWatcher fw = new FileSystemWatcher();
+                    fw.Path = @"C:\Program Files (x86)";//Path.GetDirectoryName(INF_PATH);
+                    fw.Filter = Path.GetFileName(INF_PATH);
+                    fw.IncludeSubdirectories = true;
+                    fw.Created += fileSystemWatcher_Created;
+                    fw.EnableRaisingEvents = true;
+
+                    return;
+                }
+
+                if (!File.Exists(INF_PATH + "_bak"))
+                {
+                    File.Copy(INF_PATH, INF_PATH + "_bak");
+                }
+
+
+                string stringINF = System.IO.File.ReadAllText(INF_PATH);
+                
+                //find for end for first group containing 'TosrfUsb_Device'
+                string stringSearch = "TosrfUsb.DeviceDesc";
+                
+                                
+                int positionText = 0;
+                int positionToInsert1 = 0;
+                int positionToInsert2 = 0;
+                int positionToInsert3 = 0;
+
+                positionText = stringINF.IndexOf(stringSearch, positionText + 1);
+                if (positionText > 0)
+                {
+                    positionToInsert1 = stringINF.IndexOf("\r\n\r\n", positionText);
+                    
+                }
+
+                int positionIndex = 0;
+                int positionIndexEnd = 0;
+
+                positionText = stringINF.LastIndexOf(stringSearch, positionToInsert1, positionToInsert1 - positionText);
+                positionIndex = positionText + stringSearch.Length;
+                positionIndexEnd = stringINF.IndexOf("%", positionIndex);
+
+                string positionIndexString = stringINF.Substring(positionIndex, positionIndexEnd - positionIndex);
+
+                writeLog(positionIndexString);
+
+                int deviceIndex = Int32.Parse(positionIndexString);
+
+
+                //find position for second group
+                positionText = stringINF.IndexOf(stringSearch, positionToInsert1);
+                if (positionText > 0)
+                {
+                    positionToInsert2 = stringINF.IndexOf("\r\n\r\n", positionText);
+                }
+
+                //find position for third group
+                positionText = stringINF.IndexOf(stringSearch, positionToInsert2);
+                if (positionText > 0)
+                {
+                    positionToInsert3 = stringINF.IndexOf("\r\n\r\n", positionText);
+                    if (positionToInsert3 < 0)
+                    {
+                        positionToInsert3 = stringINF.Length-2;
+                    }
+                }
+
+
+
+                string stringToAdd1 = "";
+                string stringToAdd3 = "";
+
+                for (int i = 0; i < deviceListID.Count; i++ )
+                {
+                    deviceIndex++;
+
+                    stringToAdd1 = System.Environment.NewLine + "%TosrfUsb.DeviceDesc" + deviceIndex.ToString() + "%=TosrfUsb_Device,  " +
+                    deviceListID[i];
+                    stringToAdd3 = System.Environment.NewLine + "TosrfUsb.DeviceDesc" + deviceIndex.ToString() + " = \"" +
+deviceListName[i] + "\"";
+                }
+                
+
+                //insert strings from the back
+                string stringINFNew = stringINF.Insert(positionToInsert3, stringToAdd3);
+                stringINFNew = stringINFNew.Insert(positionToInsert2, stringToAdd1);
+                stringINFNew = stringINFNew.Insert(positionToInsert1, stringToAdd1);
+
+                System.IO.File.WriteAllText(INF_PATH, stringINFNew);
+
+                writeLog("Updating INF - Complete");
+            }
+            catch (Exception ex)
+            {
+                writeLog("Updating INF - Failed : " + ex.ToString());
+            }
+        }
+
+        private void fileSystemWatcher_Created(object sender, FileSystemEventArgs e)
+        {
+            writeLog("INF file created");
+            updateINF();
+        }
+
+        private void patchRegistry()
+        {
+
+            writeLog("Patching Registry");
+
+            try
+            {
+                string keyName = @"HKEY_CURRENT_USER\Software\Toshiba\BluetoothStack\V1.0\Mng";
+                Registry.SetValue(keyName, "TestVersion", 0, RegistryValueKind.DWord);
+
+                writeLog("Patching Registry - Complete");
+            }
+            catch (Exception ex)
+            {
+                writeLog("Patching Registry - Failed : " + ex.ToString());
+            }
+        }
+
+
     }
 }
